21a6317e0089ff634b8d2a010b501726b6bb1c4d
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2021-05-03 20:39:15 +0530

    Refactor css design Removed option by clicking 3 dots

--- src/Video.js
+++ src/Video.js
@@ -1,7 +1,18 @@
+import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 import "./css/Video.css";
 export default function Video(props) {
+  const email = JSON.parse(localStorage.getItem("name"));
+
+  async function removeLikeVideo(videoId) {
+    console.log(videoId);
+    const res = await axios.post(
+      "https://Auth-API.rahulgupta99.repl.co/save/liked-videos-remove",
+      { email: email, id: videoId }
+    );
+    console.log(res);
+  }
   const { data } = props;
   let isFromLiked = null;
   if (data.snippet) {
@@ -18,30 +29,24 @@ export default function Video(props) {
   }
   if (data)
     return (
-      <div>
-        <div className="video-card-desktop">
-          <img alt="" className="video-thumbnail" src={url}></img>
-          <div className="video-info">
-            <Link to={`/video/${videoId}`} className="video-text">
-              <p className="video-text">{title}</p>
-              <small className="credits">{channelTitle}</small>
-              <small className="date">{publishedAt}</small>
-            </Link>
-          </div>
-        </div>
-        {isFromLiked && (
-          <div>
-            <label className="option-container" htmlFor="option">
+      <div className="video-card-desktop">
+        <img alt="" className="video-thumbnail" src={url}></img>
+        <div className="video-info">
+          <Link to={`/video/${videoId}`} className="video-text">
+            <p className="video-text">{title}</p>
+            <small className="credits">{channelTitle}</small>
+            <small className="date">{publishedAt}</small>
+          </Link>
+          {isFromLiked && (
+            <label
+              onClick={() => removeLikeVideo(videoId)}
+              className="option-container"
+            >
               {" "}
               &#8942;{" "}
             </label>
-            <input type="checkbox" id="option" />
-            <div className="options">
-              <p className="option-select1">Remove </p>
-              <p className="option-select2">Add to PlayList</p>
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     );
   else {

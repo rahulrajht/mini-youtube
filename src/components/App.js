934ab0dc49cbf10d8ec14e9960c23fcaaa4d6033
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2022-01-18 18:07:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,9 @@ export default function App() {
         <Route exact path="/login" component={Login} />
         <Route exact path="/playlist" component={PlayList} />
         <Route exact path="/javascript" component={Home} />
+        <Route exact path="/neog" component={Home} />
+        <Route exact path="/react" component={Home} />  
+        <Route exact path="/java" component={Home} />
         <PrivateRoute exact path="/liked-videos" component={LikedVideos} />
         <Route exact path="*" component={Error} />
       </Switch>
